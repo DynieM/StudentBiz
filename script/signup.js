329667f565bc62d49150cc2acf0dcfa5eabf3732
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-05 05:27:35 +0000

    Mar 5, 2024, 12:27 AM

---
+++ script/signup.js
@@ -0,0 +1,6 @@
+/** @format */
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ufszhsluvumwklqoxtax.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
