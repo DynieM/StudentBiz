b9a0ea4862db6768e025beac700b4b87095ecd96
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-05 06:03:53 +0000

    Mar 5, 2024, 1:03 AM

--- script/signup.js
+++ script/signup.js
@@ -4,12 +4,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ufszhsluvumwklqoxtax.supabase.co";
 const supabaseKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmc3poc2x1dnVtd2tscW94dGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkxNTY3NjAsImV4cCI6MjAyNDczMjc2MH0.is0LfBvWwr63vwRYyNPeT-2JVtGn0wtCTR__0CJqBAs";
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supaClient = createClient(supabaseUrl, supabaseKey);
 
 const googleSignIn = document.getElementById("GoogleSignUp")
 
 googleSignIn.addEventListener("click", () => {
-    supabase.auth.signInWithOAuth({
+    supaClient.auth.signInWithOAuth({
         provider: "google",
     });
 });
