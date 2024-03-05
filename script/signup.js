efdf81661558d411ae0f2c4d1cb3edd2a6e743bd
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-05 05:50:45 +0000

    Mar 5, 2024, 12:50 AM

--- script/signup.js
+++ script/signup.js
@@ -4,3 +4,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ufszhsluvumwklqoxtax.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+const googleSignIn = document.getElement
