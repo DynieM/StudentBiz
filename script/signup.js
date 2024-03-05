8b3bfacb690d55ca27f879f274cc976a379bd024
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-05 06:09:44 +0000

    Mar 5, 2024, 1:09 AM

--- script/signup.js
+++ script/signup.js
@@ -1,6 +1,9 @@
 /** @format */
 
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
+
+const { createClient } = supabase;
+
 const supabaseUrl = "https://ufszhsluvumwklqoxtax.supabase.co";
 const supabaseKey =
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmc3poc2x1dnVtd2tscW94dGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkxNTY3NjAsImV4cCI6MjAyNDczMjc2MH0.is0LfBvWwr63vwRYyNPeT-2JVtGn0wtCTR__0CJqBAs";
