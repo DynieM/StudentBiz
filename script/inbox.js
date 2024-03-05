144b072a9b122811641aabfacdc5263ccf258e63
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-03-05 03:47:26 +0000

    Mar 4, 2024, 10:47 PM

--- script/inbox.js
+++ script/inbox.js
@@ -9,3 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("inbox-container").style.display = "none";
   });
 });
+
+function showInbox() {
+        if(document.getElementById('inboxMessages').style.display == 'block'){
+            document.getElementById('inboxMessages').style.display = 'none';
+            document.getElementById('newMessageForm').style.display = 'none';
+            document.getElementById('sendMsg').style.display = 'none';
+        } else {
+            document.getElementById('inboxMessages').style.display = 'block';
+            document.getElementById('sendMsg').style.display = 'block';
+            document.getElementById('newMessageForm').style.display = 'none';
+        }
+    }
+
+    function showNewMessageForm() {
+        document.getElementById('inboxMessages').style.display = 'none';
+        document.getElementById('sendMsg').style.display = 'none';
+        document.getElementById('newMessageForm').style.display = 'block';
+    }
