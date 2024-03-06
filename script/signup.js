fdb14a197e968fd8dd8e59e0f37c3652f2febef6
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-06 04:49:40 +0000

    Mar 5, 2024, 11:49 PM

--- script/signup.js
+++ script/signup.js
@@ -66,11 +66,11 @@ content.style.visibility = "hidden"
 is_Student.addEventListener("change", () => {
     if (is_Student.checked) {
         content.style.visibility = "visible"
-       sign_up_button.style.cssText = "margin-top: 0px !important;";
+        sign_up_button.style.marginTop = "0px";
         
     } else {
         content.style.visibility = "hidden"
-        sign_up_button.style.cssText = "margin-top: -500px !important;";
+        sign_up_button.style.marginBottom = "600px";
     }
 
 })
