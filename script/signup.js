49e55a7b527dae56e2a98b1a0d3918f2b56d5d42
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-03-06 01:26:02 +0000

    Mar 5, 2024, 8:26 PM

--- script/signup.js
+++ script/signup.js
@@ -34,7 +34,8 @@ async function submitForm() {
 
         const { data, error } = await supabase
             .from('users')
-            .insert({ id: id, first_name_db: first_name, last_name_db: last_name});
+            .insert({ id: id, first_name_db: first_name, last_name_db: last_name });
+            
             
     }
 
