fcb8c684affb5d226ae6955f4f15d60310c56933
Yvonne Aime <yvonneaime0@gmail.com>
2024-03-08 02:03:05 +0000

    Mar 7, 2024, 9:03 PM

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const port = 3001
 // allow json to be sent
 app.use(express.json());
 
+const cors = require('cors');
+
 // db info
 const supabaseUrl = "https://ufszhsluvumwklqoxtax.supabase.co";
 const SB = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmc3poc2x1dnVtd2tscW94dGF4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTE1Njc2MCwiZXhwIjoyMDI0NzMyNzYwfQ.4sAKoiqIDkMaRcy9RKIKTtlBSAn2Y3PXxM8dh_LBMqQ";
