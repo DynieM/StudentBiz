86062968745963d7334da0219584bfeecd63218b
Yvonne Aime <yvonneaime0@gmail.com>
2024-03-08 01:16:42 +0000

    Mar 7, 2024, 8:16 PM

--- script/app.js
+++ script/app.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/adduser', (req, res) => {
+    const { data, error } = await supabase
+        .from("users")
+        .insert({
+            first_name_db: first_name,
+            last_name_db: last_name,
+            email_db: email,
+            phone_number_db: phone_number,
+            password_db: password,
+
+        });
 })
 
 app.listen(port, () => {
