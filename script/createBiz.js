ecb27caaf58f457f04e768cd91042c5be80c10aa
jmsalim <jmsalimknights@gmail.com>
2024-03-25 19:53:29 +0000

    Mar 25, 2024, 3:53 PM

--- script/createBiz.js
+++ script/createBiz.js
@@ -8,6 +8,8 @@ const supabaseKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmc3poc2x1dnVtd2tscW94dGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkxNTY3NjAsImV4cCI6MjAyNDczMjc2MH0.is0LfBvWwr63vwRYyNPeT-2JVtGn0wtCTR__0CJqBAs";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+document.getElementById("addBusiness").addEventListener("click", async function (event) 
+
 // Collect input values
 const business_email = document.getElementById("email").value;
 const business_description = document.getElementById("description").value;
