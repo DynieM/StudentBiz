abaf2128c931dfce8a3895256162882df937316e
Yvonne Aime <yvonneaime0@gmail.com>
2024-03-27 21:09:43 -0400

    user info

--- script/userProfile.js
+++ script/userProfile.js
@@ -1,10 +1,25 @@
 /** @format */
 
 window.onload = function () {
-	const firstName = localStorage.getItem("firstName");
-    const email = localStorage.getItem("email");
-    const phone = localStorage.getItem("phone")
-	document.querySelector(".username-text").textContent = firstName;
+    let firstName = localStorage.getItem("firstName");
+    let email = localStorage.getItem("email");
+    let phone = localStorage.getItem("phone");
+    // Displays user profile info
+    if (firstName == null || firstName == "") {
+        let firstName = document.querySelector(".username-text").textContent;
+        localStorage.setItem("firstName", firstName);
+    }
+    if (email == null || email == "") {
+        let email = document.querySelector(".emailinfo").textContent;
+        localStorage.setItem("email", email);
+    }
+    if (phone == null || phone == "") {
+        let phone = document.querySelector(".phoneinfo").textContent;
+        localStorage.setItem("phone", phone);
+    }
+
+
+    document.querySelector(".username-text").textContent = firstName;
     document.querySelector(".emailinfo").textContent = email;
     document.querySelector(".phoneinfo").textContent = phone;
 };
