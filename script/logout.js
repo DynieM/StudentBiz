518facd0c613fdc6abf0684f8f6439823e8da5b3
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-03-28 19:14:29 +0000

    Mar 28, 2024, 3:14 PM

--- script/logout.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    let logout = document.getElementById('logoutButton');
-    logout.addEventListener('mouseover', function() {
-        logout.style.display = 'block';
-    });
-}
