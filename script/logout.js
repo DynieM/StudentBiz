68f5282ef00b588c360dacd4e3170dac4e277b4c
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-03-29 19:45:32 +0000

    Mar 29, 2024, 3:45 PM

--- script/logout.js
+++ script/logout.js
@@ -2,7 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     function handleHover() {
     const profilePicIcon = document.getElementById('profilepicicon');
     const logoutButton = document.getElementById('logoutButton');
+    let logoutButtonLink = document.getElementById('logoutLink');
     let timeout;
+
+    if (logoutButton.style.display === 'block') {
+        logoutButtonLink.href = '../html/login.html';
+    } else {
+        logoutButtonLink.href = '#';
+    }
 
     profilePicIcon.addEventListener('mouseenter', () => {
         logoutButton.style.display = 'block';
